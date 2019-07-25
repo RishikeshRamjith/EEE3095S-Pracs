db9dec6032ec72dd0449c063a8a5342291b1d9f2
Kesh <hramjith@gmail.com>
2019-07-25 07:57:32 +0100

    LED now blinks

--- PracSource/Prac1/template.py
+++ PracSource/Prac1/template.py
@@ -11,18 +11,20 @@ Date: 25/07/2019
 
 # import Relevant Librares
 import RPi.GPIO as GPIO
-
+import time
 # Logic that you write
 def main():
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(18, GPIO.OUT)
     GPIO.output(18, GPIO.HIGH)
-
+    time.sleep(1)
+    GPIO.output(18, GPIO.LOW)
+    time.sleep(1)
 
 # Only run the functions if 
 if __name__ == "__main__":
     # Make sure the GPIO is stopped correctly
     try:
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(18, GPIO.OUT)
         while True:
             main()
     except KeyboardInterrupt:
